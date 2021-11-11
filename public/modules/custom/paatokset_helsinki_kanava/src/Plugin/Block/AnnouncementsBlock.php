cfd249821e5124ae1f01d31f1ea6d76229579828
Jere Ljungberg <jere@mysteerio.fi>
2021-11-11 10:03:20 +0200

    Accessibility check-specific changes

--- public/modules/custom/paatokset_helsinki_kanava/src/Plugin/Block/AnnouncementsBlock.php
+++ public/modules/custom/paatokset_helsinki_kanava/src/Plugin/Block/AnnouncementsBlock.php
@@ -63,7 +63,7 @@ class AnnouncementsBlock extends BlockBase {
    * Return true if it is less than one day to the next meeting.
    */
   private function shouldShowAlert($time) {
-    return strtotime('+1 day') > strtotime($time);
+    return TRUE;
   }
 
 }
